13944bf50941624b2ea972da20355d36d78f13d7
Benjamin PAGES <bpages@MLHR.com>
2022-04-05 09:33:10 +0200

    ajout modification de profil

--- components/Profile.vue
+++ components/Profile.vue
@@ -1,42 +1,122 @@
 <template>
     <div>
         <b-row class="mb-3 py-5 icon-background">
-            <b-col cols='6' offset='1'>
-                <div class="profil-img-container">
-                    <b-img :src="profileLogged.imageURL" thumbnail class="profile-img" alt="Profile Image"></b-img>        
+            <b-col cols='6' class="d-flex flex-column justify-content-center align-items-center">
+                <div class="profil-img-container d-flex justify-content-center">
+                    <b-img :src="profile.imageURL" thumbnail class="profile-img" alt="Profile Image"></b-img>        
                 </div>
-
                 
-
-                <!-- <div class="change-profile-img">
-                    <label for="change-profile-img-input" class="for">
+                <b-button v-if="owner && !modify" variant="outline-primary" class="m-2" @click="modifyForm()">Modifier le profil</b-button>
+                
+                <div v-if="modify && form.file == null" class="change-profile-img">
+                    <label for="change-profile-img-input">
                         <b-icon icon="upload" variant="dark" font-scale="1"></b-icon>
                         <span class="dark small">Changer d'image</span>
                     </label>
-                    <input id="change-profile-img-input" type="file">
-                </div> -->
+                    <b-form-file plain accept=".jpg, .jpeg, .png" id="change-profile-img-input" v-model="form.file"></b-form-file>
+                </div>
+                <span class="small text-danger" v-if="modify && form.file != null" @click="form.file = null">
+                    <b-icon icon="x-circle" variant="danger" font-scale="1"></b-icon>
+                    Annuler changement
+                </span>
+
             </b-col>
         </b-row>
-        <b-row class="py-3 border-bottom">
-            <b-badge v-if="this.$store.state.profileLogged.admin" variant="danger">Admin</b-badge>
+
+        <b-row v-if="!modify" class="py-3 border-bottom">
+            <b-badge v-if="profile.admin" variant="danger">Admin</b-badge>
+
             <b-list-group class="w-100">
+
                 <b-list-group-item class="d-flex justify-content-between align-items-center">
                     <span>Prénom :</span>
-                    <span>{{profileLogged.firstname.toUpperCase()}}</span>
+                    <span>{{profile.firstname}}</span>
                 </b-list-group-item>
+
                 <b-list-group-item class="d-flex justify-content-between align-items-center">
                     <span>Nom :</span>
-                    <span>{{profileLogged.lastname.toUpperCase()}}</span>
+                    <span>{{profile.lastname}}</span>
                 </b-list-group-item>
+
                 <b-list-group-item class="d-flex justify-content-between align-items-center">
                     <span>Poste :</span>
-                    <span>{{profileLogged.position}}</span>
+                    <span>{{profile.position}}</span>
                 </b-list-group-item>
+
                 <b-list-group-item class="d-flex justify-content-between align-items-center">
                     <span>contact :</span>
-                    <span>{{profileLogged.email}}</span>
+                    <span>{{profile.email}}</span>
                 </b-list-group-item>
+
             </b-list-group>
+
+        </b-row>
+
+        <b-row v-if="modify" class="pt-3 border-bottom w-100 m-0 p-0">
+
+            <b-form class="w-100">
+                <b-row cols="2" class="w-100 bg-white border rounded mx-0 mb-1 p-2">
+                    <b-col class="d-flex align-items-center">
+                        <label class="m-0" for="firstname">Prénom : </label>
+                    </b-col>
+                    <b-col>
+                        <b-form-input
+                        id="firstname"
+                        class="mb-1 mr-sm-1 mb-sm-0"
+                        v-model="form.user.firstname"
+                        ></b-form-input>
+                    </b-col>
+                </b-row>
+
+                <b-row cols="2" class="w-100 bg-white border rounded mx-0 mb-1 p-2">
+                    <b-col class="d-flex align-items-center">
+                        <label class="m-0" for="lastname">Nom :</label>
+                    </b-col>
+                    <b-col>
+                        <b-form-input
+                        id="lastname"
+                        class="mb-1 mr-sm-1 mb-sm-0"
+                        v-model="form.user.lastname"
+                        ></b-form-input>
+                    </b-col>
+                </b-row>
+
+                <b-row cols="2" class="w-100 bg-white border rounded mx-0 mb-1 p-2">
+                    <b-col class="d-flex align-items-center">
+                        <label class="m-0" for="position">Poste :</label>
+                    </b-col>
+                    <b-col>
+                        <b-form-input
+                        id="position"
+                        class="mb-1 mr-sm-1 mb-sm-0"
+                        v-model="form.user.position"
+                        ></b-form-input>
+                    </b-col>
+                </b-row>
+
+                <b-row cols="2" class="w-100 bg-white border rounded mx-0 mb-1 p-2">
+                    <b-col class="d-flex align-items-center">
+                        <label class="m-0" for="mail">Adresse mail :</label>
+                    </b-col>
+                    <b-col>
+                        <b-form-input
+                        id="mail"
+                        class="mb-1 mr-sm-1 mb-sm-0"
+                        v-model="form.user.email"
+                        ></b-form-input>
+                    </b-col>
+                </b-row>
+
+                <b-row class="py-3 w-100 m-0 p-0">
+                    <b-button variant="primary" @click.prevent="modifyUserProfile()">
+                        <span v-if="!loading">modifier</span>
+                        <span v-else>
+                            <b-spinner small></b-spinner>
+                        </span>
+                        </b-button>
+                </b-row>
+            </b-form>
+
         </b-row>
 
     </div>
@@ -44,18 +124,58 @@
 </template>
 
 <script>
-    import {mapState} from 'vuex'
 
-    export default {
-    name: 'postCard',
-
-    computed : {
-        ...mapState(['profileLogged'])
+export default {
+    name: 'profile',
+    data : function () {
+        return {
+            modify : false,
+            loading : false,
+            form : {
+                file : null,
+                user : {
+                    firstname : this.profile.firstname,
+                    lastname : this.profile.lastname,
+                    position : this.profile.position,
+                    email : this.profile.email, 
+                }
+            }
+        }
     },
 
-    mounted() {
-        const userId = this.$store.state.user.userId
-        this.$store.dispatch('getUserProfile', userId)
+    props : {
+        profile : Object,
+        owner : Boolean
+    }, 
+
+    methods : {
+        modifyForm : function () {
+            this.modify = true
+        }, 
+        
+        modifyUserProfile : async function() {
+             try {
+                    this.loading = true 
+                    //données qui vont etre envoyées si il y a un fichier:
+                    const data = new FormData()
+                    data.append('file', this.form.file)
+                    data.append('user', JSON.stringify(this.form.user))
+                    
+                    const user = !this.form.file ? this.form.user : data
+                    console.log(user)
+                    console.log(this.profile.id)
+
+                    await this.$axios.put('/user/' + this.profile.id, user)
+                    
+                    this.loading = false
+                    this.modify = false
+
+                    console.log('==========Profil mis à jour==========')
+
+                } catch (e) {
+                    console.log(e)
+                }
+        }
     }
 }
 </script>
@@ -70,8 +190,8 @@
     }
 
     .profil-img-container {
-        width: 200px;
-        height: 200px;
+        width: 150px;
+        height: 150px;
         border-radius: 100%;
     }
 
@@ -82,13 +202,13 @@
         min-height: 100%;
     }
 
-    /* .change-profile-img>input {
+    .change-profile-img>input {
         display: none;
         cursor: pointer;
     }
 
-    .change-profile-img>label {
+    .change-profile-img>#change-profile-img-input {
     cursor: pointer;
-    } */
+    }
 
 </style>
--- pages/Profile.vue
+++ pages/Profile.vue
@@ -1,10 +1,23 @@
 <template>
-    <Profile/>
+    <Profile
+        :profile='profileLogged'
+        owner
+    />
 </template>
 
 <script>
+import {mapState} from 'vuex'
 
 export default {
+    name : 'myProfile', 
 
+    computed : {
+        ...mapState(['profileLogged'])
+    },
+
+    mounted() {
+        const userId = this.$store.state.user.userId
+        this.$store.dispatch('getUserProfile', userId)
+    }
 }
 </script>
