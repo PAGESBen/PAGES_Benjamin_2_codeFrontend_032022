bdbd93dce73b98832c0b602656c44db3620d9384
Benjamin PAGES <bpages@MLHR.com>
2022-04-08 16:13:11 +0200

    ajout pagination composant posts

--- components/posts.vue
+++ components/posts.vue
@@ -1,7 +1,23 @@
 <template>
     <div>
-        <PostCard v-for="post in posts" :key="post.id"
+        <!-- <PostCard v-for="post in posts" :key="post.id"
             :post="post" />
+     -->
+
+        <!--pagination -->
+        <b-row class="my-5">
+            <b-col>
+               <b-pagination
+                    v-model="page"
+                    :total-rows="posts.postsCount"
+                    :per-page="limit"
+                    size="sm"
+                    align="center"
+                    class="my-5"
+                    @change="nextPage"
+                    ></b-pagination>
+            </b-col>
+        </b-row>
     </div>
 </template>
 
@@ -9,16 +25,29 @@
 export default {
     data() {
         return {
-            posts : []
+            posts : {},
+            page : 1,
+            limit : 20
         }
     }, 
 
     async mounted() {
         try {
-            this.posts = await this.$axios.$get('/post/' + 1 + '/' + 10)
+            this.posts = await this.$axios.$get('/post/' + this.page + '/' + this.limit)
             console.log(this.posts)
         } catch (e) {
             console.log(e)
+        }
+    },
+
+    methods : {
+        async nextPage() {
+            try {
+                this.posts = await this.$axios.$get('/post/' + this.page + '/' + this.limit)
+                console.log(this.posts)
+            } catch (e) {
+                console.log(e)
+            }    
         }
     }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,5 @@
 <template>
-  <div>
-    <b-row class="d-flex justify-content-center">
-      <b-col>
-        <div class="overflow-auto">
-          <b-pagination-nav :link-gen="linkGen" :number-of-pages="10" use-router></b-pagination-nav>
-        </div>
-      </b-col>
-    </b-row>
-
-      <Posts/>
-
-    <b-row class="d-flex justify-content-center">
-      <b-col>
-        <div class="overflow-auto">
-          <b-pagination-nav :link-gen="linkGen" :number-of-pages="10" use-router></b-pagination-nav>
-        </div>
-      </b-col>
-    </b-row>
-    
-  </div>
+  <Posts/>
 </template>
 
 <script>
